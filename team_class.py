8c4336fe6d81199f2da88669b0af347f1ee70033
Snnaus <nausleyg@gmail.com>
2014-09-01 14:47:54 -0400

    Initial Team Class commit

---
+++ team_class.py
@@ -0,0 +1,6 @@
+
+
+class Team():
+    '''This class is for the teams in the game; this is going to more or less a storage place for all the information pertaining to the team;
+    The decision making will be found in the coaches; the 'playing' will be found in... well... the players; this will hold the list of players, coach,
+    board members, and financials of the team.'''
